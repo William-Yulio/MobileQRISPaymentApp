8f942fe00821d07382664e1c9c4e85057851ec1c
William-Yulio <william.yulio331@gmail.com>
2023-08-05 17:32:57 +0700

    Initial Commit

---
+++ Mobile Payment QRIS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile Payment QRIS
+//
+//  Created by William Yulio on 05/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
